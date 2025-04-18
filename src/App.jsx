46ebc3e50eb8394f20fe7a5dd0986a3fd8508c3b
ezequiel <ezequielwanzelerdeoliveira@gmail.com>
2025-04-18 17:50:19 -0300

    adicionando uma função que fala se a tarefa foi completa ou não

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,20 @@ function App() {
     name: "Estudar TypeScript",
     done: false,
   }])
+  function ontask(Taskid) {
+    const newtasks = taskk.map((task) => {
+      if(task.id === TaskId) {
+        return({...task, done: !task.done})
+      }
+      return task;
+    })
+    setTaskk(newtasks)
+  }
 return(
   <div className="w-screen h-screen bg-slate-500 flex items-center p-6 flex-col">
   <div className="w-[500px]"><h1 className="text-3xl text-center text-slate-100 flex-col">Gerenciador de Tarefas</h1>
 </div>
-    <Tasks taskk={taskk} />
+    <Tasks taskk={taskk} ontask={ontask}/>
   </div>
 )
 
--- src/componets/Tasks.jsx
+++ src/componets/Tasks.jsx
@@ -6,7 +6,7 @@ function Tasks(props) {
         {props.taskk.map((task) => (
             <li key={task.id} className="flex gap-2">
 
-                <button className='bg-slate-600 rounded-md p-2 text-white w-full text-left'>{task.name}</button>
+                <button onclick={() => props.ontask(task.id)} className='bg-slate-600 rounded-md p-2 text-white w-full text-left'>{task.name}</button>
                 <button className="bg-slate-600 rounded-md text-white p-2">
                     <ChevronRight />
                 </button>
